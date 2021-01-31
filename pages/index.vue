778c0561cbd073df817ca3e6f5805fe647f49c66
Your Name <you@example.com>
2021-01-31 11:03:37 +0900

    学習の記録のためにアップロードしました

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>中野サンプラザ</h3>
+    <img src="~/assets/20201227_135018.jpg">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  img {
+  width: 600px;
+  height: 600px;
+ }
+</style>
